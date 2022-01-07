3a5a04ec961c7c05361359b3500a2c4e942f522b
hafizshadm <96694805+hafizshadm@users.noreply.github.com>
2022-01-07 18:11:57 +0500

    JavaScript is ongoing

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,5 @@
-const playerConfigOverlayElement = document.getElementById("config-overlay");
-const backDropElement = document.getElementById("backdrop");
-const formElement = document.querySelector("form");
-const playerName = document.getElementById("player-name");
-
-const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -13,18 +8,30 @@ const players = [
   },
   {
     name: "",
-    symbol: "X",
+    symbol: "O",
   },
 ];
 
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backDropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const playerName = document.getElementById("player-name");
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const errorsOutputElement = document.getElementById("config-error");
-
 const cancelBtnConfig = document.getElementById("cancel-player-config");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameBoard = document.getElementById("active-game");
+// const gameFiledElements = document.querySelectorAll("#game-board li");
+const gameBoardElement = document.getElementById("game-board");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
-
 cancelBtnConfig.addEventListener("click", closeBtnConfig);
 backDropElement.addEventListener("click", closeBtnConfig);
 formElement.addEventListener("submit", savePlayerConfig);
+startNewGameBtnElement.addEventListener("click", startNewGame);
+// for (const gameFiledElement of gameFiledElements) {
+//   gameFiledElement.addEventListener("click", selectGameField);
+// }
+gameBoard.addEventListener("click", selectGameField);
---
+++ scripts/game.js
@@ -0,0 +1,21 @@
+function startNewGame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set player names for both players!");
+    return;
+  }
+  gameBoard.style.display = "block";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+}
+
+function selectGameField(event) {
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add("disabled");
+  switchPlayer();
+}
